1491105fbb160237de4ab8856a2a488d3aebc0a9
QK04 <khanhnq.22bi13211@usth.edu.vn>
2024-12-17 20:54:20 +0700

    change file name css to make it correct

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './register.css'; // Scoped CSS
+import './register.css'; 
 import axios from 'axios';
 import usthLogo from '../assets/usthlogo.png';
 import styled from '@emotion/styled';
