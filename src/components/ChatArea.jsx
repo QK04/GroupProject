d6f74d3aba322f6aedbbce5e3865796b443d5beb
Okdat <155612361+Okdat@users.noreply.github.com>
2025-02-03 15:28:22 +0700

    Update ChatArea.jsx

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -136,7 +136,7 @@ const ChatArea = () => {
 
         if (response.status === 200) {
           const botResponse = await fetch(
-            ' https://8c63-42-113-20-81.ngrok-free.app/api/message',
+            ' https://d8eb-42-113-20-81.ngrok-free.app/api/message',
             {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
