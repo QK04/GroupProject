88e71e8db02f332d8b9f5fd0f9f93f3ffb2f192d
Okdat <155612361+Okdat@users.noreply.github.com>
2025-02-03 14:36:02 +0700

    Update ChatArea.jsx

--- src/components/ChatArea.jsx
+++ src/components/ChatArea.jsx
@@ -17,7 +17,7 @@ const ChatArea = ({ currentChat, messages, onSendMessage, isOpen }) => {
       setInput('');
 
       try {
-       const response = await fetch('   https://dfaa-42-113-20-81.ngrok-free.app/api/message', { 
+       const response = await fetch('https://8c63-42-113-20-81.ngrok-free.app/api/message', { 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: input }),
